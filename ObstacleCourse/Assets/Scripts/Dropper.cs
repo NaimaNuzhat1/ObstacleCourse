17625f44919f566ca98e17438f15e30a989fb6a6
NaimaNuzhat1 <naimanuzhat@gmail.com>
2024-05-17 20:38:47 +0500

    .

--- ObstacleCourse/Assets/Scripts/Dropper.cs
+++ ObstacleCourse/Assets/Scripts/Dropper.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] float timeToWait= 5f;
+    MeshRenderer renderer;
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>();
+        renderer.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > timeToWait)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.useGravity = true; 
+            renderer.enabled = true;
+        }
     }
 }
